fb059793fd34a001cd1a1dc653029c7a28c7bc06
Nicholas Bishop <nicholasbishop@gmail.com>
2015-04-19 20:15:54 -0400

    Automatic-ish rustdoc update

--- doc/implementors/core/ops/trait.Add.js
+++ doc/implementors/core/ops/trait.Add.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['dumbmath'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Add.html' title='core::ops::Add'>Add</a> for <a class='struct' href='dumbmath/vector/struct.Vec3f.html' title='dumbmath::vector::Vec3f'>Vec3f</a>",];
+implementors['dumbmath'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Add.html' title='core::ops::Add'>Add</a> for <a class='struct' href='dumbmath/vector/struct.Vec3f.html' title='dumbmath::vector::Vec3f'>Vec3f</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
