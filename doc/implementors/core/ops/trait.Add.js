b2c06e948d36c7554ffe89f0a359b638881e7e73
Nicholas Bishop <nicholasbishop@gmail.com>
2015-05-04 02:03:55 -0400

    Automatic-ish rustdoc update

--- doc/implementors/core/ops/trait.Add.js
+++ doc/implementors/core/ops/trait.Add.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['dumbmath'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Add.html' title='core::ops::Add'>Add</a> for <a class='struct' href='dumbmath/vector/struct.Vec3f.html' title='dumbmath::vector::Vec3f'>Vec3f</a>",];
+implementors['dumbmath'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Add.html' title='core::ops::Add'>Add</a>&lt;<a class='struct' href='dumbmath/vector/struct.Vec3f.html' title='dumbmath::vector::Vec3f'>Vec3f</a>&gt; for <a class='struct' href='dumbmath/segment/struct.Segment3f.html' title='dumbmath::segment::Segment3f'>Segment3f</a>","impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Add.html' title='core::ops::Add'>Add</a> for <a class='struct' href='dumbmath/vector/struct.Vec3f.html' title='dumbmath::vector::Vec3f'>Vec3f</a>","impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Add.html' title='core::ops::Add'>Add</a>&lt;<a href='http://doc.rust-lang.org/nightly/std/primitive.f32.html'>f32</a>&gt; for <a class='struct' href='dumbmath/vector/struct.Vec3f.html' title='dumbmath::vector::Vec3f'>Vec3f</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
